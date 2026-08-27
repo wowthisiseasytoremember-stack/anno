0ccc44675a5436e3148b617144055ca80fd3d117
Justin <wowthisiseasytoremember@gmail.com>
2026-08-27 23:42:42 +0000

    TodayView: replace inline Color(hex:) with AnnoTheme.rose token

--- Anno/Today/TodayView.swift
+++ Anno/Today/TodayView.swift
@@ -161,7 +161,7 @@ struct TodayView: View {
         case "blue", "lapis", "xanh":                 return AnnoTheme.lapis
         case "green":                                  return AnnoTheme.verdigris
         case "violet", "purple":                      return AnnoTheme.advent
-        case "rose":                                   return Color(hex: 0xB3666E)
+        case "rose":                                   return AnnoTheme.rose
         case "black":                                  return AnnoTheme.ash
         default:                                       return AnnoTheme.incense
         }
